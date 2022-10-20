bc4434caec057704d1cdc1f31ab0474f0146b801
korean_kat <katarina999@outlook.fr>
2022-10-20 15:24:49 +0100

    new workout form logic

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 
 //components
 import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
   const [workouts, setWorkouts] = useState(null)
@@ -23,6 +24,7 @@ const Home = () => {
             <WorkoutDetails key={workout._id} workout={workout} />
           ))}
       </div>
+      <WorkoutForm />
     </div>
   )
 }
