22707cd44ac0c75f508cd9e2919b075d0ea75e76
korean_kat <katarina999@outlook.fr>
2022-10-20 16:29:56 +0100

    adding react context

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,31 +1,37 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 
-//components
+// components
 import WorkoutDetails from "../components/WorkoutDetails"
 import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
-  const [workouts, setWorkouts] = useState(null)
+  const { workouts, dispatch } = useWorkoutsContext()
+
   useEffect(() => {
     const fetchWorkouts = async () => {
       const response = await fetch("/api/workouts")
       const json = await response.json()
+
       if (response.ok) {
-        setWorkouts(json)
+        dispatch({ type: "SET_WORKOUTS", payload: json })
       }
     }
+
     fetchWorkouts()
-  }, [])
+  }, [dispatch])
+
   return (
     <div className='home'>
       <div className='workouts'>
         {workouts &&
           workouts.map((workout) => (
-            <WorkoutDetails key={workout._id} workout={workout} />
+            <WorkoutDetails workout={workout} key={workout._id} />
           ))}
       </div>
       <WorkoutForm />
     </div>
   )
 }
+
 export default Home
