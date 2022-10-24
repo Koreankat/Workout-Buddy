9300b55b5c76c86a0ee9f387b0f16434454f8bfb
korean_kat <katarina999@outlook.fr>
2022-10-24 13:38:15 +0100

    tet

--- frontend/src/context/WorkoutsContext.js
+++ frontend/src/context/WorkoutsContext.js
@@ -4,10 +4,9 @@ export const WorkoutsContext = createContext()
 
 export const workoutsReducer = (state, action) => {
   switch (action.type) {
-    case "SET_WORKOUTS":
-      return {
-        workouts: action.payload,
-      }
+    case "SET_WORKOUTS": {
+      return { workouts: action.payload }
+    }
     case "CREATE_WORKOUT":
       return {
         workouts: [action.payload, ...state.workouts],
