8d31f80d3fcd7c3d63b2012b668a6f08256178f1
korean_kat <katarina999@outlook.fr>
2022-12-23 10:05:45 +0100

    useLogout hook

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom"
+import { useLogout } from "../hooks/useLougout"
 
 const Navbar = () => {
+  const { logout } = useLogout()
+  const handleClick = () => {
+    logout()
+  }
   return (
     <header>
       <div className='container'>
         <Link to='/'>
           <h1> Workout Buddy</h1>
         </Link>
+
         <nav>
+          <div>
+            <button onClick={handleClick}>Log out</button>
+          </div>
           <div>
             <Link to='/login'>Login</Link>
             <Link to='/signup'>Sign up</Link>
---
+++ frontend/src/hooks/useLougout.js
@@ -0,0 +1,13 @@
+import { useAuthContext } from "./useAuthContext"
+
+export const useLogout = () => {
+  const { dispatch } = useAuthContext()
+  const logout = () => {
+    //remove user from localstorage
+    localStorage.removeItem("user")
+
+    //dispatch lougout action
+    dispatch({ type: "LOGOUT" })
+  }
+  return { logout }
+}
